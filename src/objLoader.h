e7fe4dfc5f65abdc6ef4c0a7f060e62d11213dff
Kyle Kolpek <kakolpek@gmail.com>
2012-04-09 18:28:06 -0400

    Cleaned up formatting

--- src/objLoader.h
+++ src/objLoader.h
@@ -42,54 +42,57 @@ struct ObjInfo
 };
 
 /***************************************************************************//**
-* Loads information from Wavefront obj files.
-* Gets obj file and parsers out the needed information to be able to display
-* correct model on screen using OpenGL. To use all you have to do is create an
-* instance of ObjLoader and call the method getData(..) with appropriate params
-* and it will return the information.
-******************************************************************************/
+ * Loads information from Wavefront obj files.
+ * Gets obj file and parsers out the needed information to be able to display
+ * correct model on screen using OpenGL. To use all you have to do is create an
+ * instance of ObjLoader and call the method getData(..) with appropriate params
+ * and it will return the information.
+ ******************************************************************************/
 
 class ObjLoader
 {
-	public:
-		/***************************************************************************//**
-		* Default Constructor.
-		******************************************************************************/
-		ObjLoader();
+public:
+	/***********************************************************************//**
+	 * Default Constructor.
+	 **************************************************************************/
+	ObjLoader();
 
-		/***************************************************************************//**
-		* Default Destructor.
-		******************************************************************************/
-		~ObjLoader();
+	/***********************************************************************//**
+	 * Default Destructor.
+	 **************************************************************************/
+	~ObjLoader();
 
-		/***************************************************************************//**
-		* Loads obj file and parses out the information.(Moved to private maybe?)
-		* \param[in] filename
-		*     The filename of the obj file to load.
-		******************************************************************************/
-		void load(std::string filename);
+	/***********************************************************************//**
+	 * Loads obj file and parses out the information.(Moved to private maybe?)
+	 * \param[in] filename
+	 *     The filename of the obj file to load.
+	 **************************************************************************/
+	void load(std::string filename);
 
-		/***************************************************************************//**
-		* Gets appropriated data, and returns the data with vertexData, indexData and type
-		* \param[in] filename
-		* 	  The filename of the obj file to load.
-		* \param[out] vertexData
-		*     The complete vertex data in the form of pos1, norm1, tex1, ... , posN, normN, texN
-		* \param[out] indexData
-		*     The index face data. Probably not necessary.
-		* \param[out] type
-		* 	  GL type that tells the receiver what kind of shape the model is made of.
-		******************************************************************************/
-		void getData(std::string filename, 
-					float* vertexData, 
-					int* indexData,
-					GLuint type);
+	/***********************************************************************//**
+	 * Gets appropriated data, and returns the data with vertexData, indexData
+	 * and type.
+	 * \param[in] filename
+	 * 	  The filename of the obj file to load.
+	 * \param[out] vertexData
+	 *     The complete vertex data in the form of pos1, norm1, tex1, _ ,
+	 *     posN, normN, texN.
+	 * \param[out] indexData
+	 *     The index face data. Probably not necessary.
+	 * \param[out] type
+	 * 	  GL type that tells the receiver what kind of shape the model is made
+	 *    of.
+	 **************************************************************************/
+	void getData(std::string filename, 
+				float* vertexData, 
+				int* indexData,
+				GLuint type);
 
-	private:
-		/***************************************************************************//**
-		* Holds the data from the obj for later conversion to output format.
-		******************************************************************************/
-		ObjInfo* data;
-		
+private:
+	/***********************************************************************//**
+	 * Holds the data from the obj for later conversion to output format.
+	 **************************************************************************/
+	ObjInfo* data;
+	
 };
 #endif 
