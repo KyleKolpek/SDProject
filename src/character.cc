8ea036374074a1de136db8739510a6c7a70aded7
Kyle Kolpek <kakolpek@gmail.com>
2012-04-12 02:05:24 -0400

    Implemented the drawing for Character

--- src/character.cc
+++ src/character.cc
@@ -4,28 +4,41 @@ Character::Character(Camera *camera):
 	x(0),
 	y(0),
 	z(-17),
-	vertexDataSize(0),
+	vertexCount(0),
 	vertexData(NULL),
 	vertexBuffer(NULL),
 	camera(camera)
 {
 	ObjLoader loader();
-	loader.getData("../assets/models/Legoman/Legoman.obj",
+	loader.getData("../assets/models/Legoman/LegoMan.obj",
 				   vertexData,
 				   indexData,
 				   dataDype,
 				   vertexCount);
+
+    // Prepare vertex buffer
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    // Add vertedData to a buffer
+    glBufferData(GL_ARRAY_BUFFER, vertexCount*sizeof(float), vertexData,
+        GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void Character::draw()
 {
+	// TODO: Store a ShaderManager over a program
 	glUseProgram(program);
 
 	GLint mvLoc = glGetUniformLocation(program, "modelView");
 	GLint nmvLoc = glGetUniformLocation(program, "normalModelView");
+	GLint projLoc = glGetUniformLocation(program, "projection");
 
 	glm::mat4 MV = getModelMatrix() * camera.getViewMatrix();
+	glm::mat4 proj = camera.getProjectionMatrix();
 
+	// TODO: Error check here.
 	if(mvLoc != -1)
 	{
 		glUniformMatrix4v(mvLoc, 1, GL_FALSE, &MV[0][0]);
@@ -37,16 +50,24 @@ void Character::draw()
 			glm::transpose(glm::inverse(&MV[0][0])));
 	}
 
+	if(projLoc != -1)
+	{
+		glUniformMatrix4v(mvLoc, 1, GL_FALSE, &projection[0][0]);
+	}
+
 	GLuint vertexPosLoc = glGetAttribLocation(program, "position");
 	GLuint vertexNormalLoc = glGetAttribLocation(program, "normal");
 
-	glBindBuffer(GL_ARRAY_BUFFER);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glVertexAttribPointer(vertexPosLoc, 3, GL_FLOAT, GL_FALSE,
 		8 * sizeof(float), 0);
 	glVertexAttribPointer(vertexNormalLoc, 3, GL_FLOAT, GL_FALSE,
 		8 * sizeof(float), (void *)(3 * sizeof(float)));
 
-	glDrawArray(GL_TRIANGLES, 0, vertexDataCount);
+	glDrawArray(GL_TRIANGLES, 0, vertexCount);
+	glDisableVertexAttribArray(vertexPosLoc);
+	glDisableVertexAttribArray(vertexNormalLoc);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 glm::mat4 getModelMatrix()
