528b12903a18bddfefde9a0b224926e80e4dac2b
Mirza <mprasovic@gmail.com>
2012-04-21 15:23:15 -0400

    Added the Wall class.

---
+++ src/wall.cc
@@ -0,0 +1,20 @@
+#include "wall.h"
+
+Wall::Wall(float x_1, float y_1, float x_2, float y_2)
+{
+	x1 = x_1;
+	y1 = y_1;
+	x2 = x_2;
+	y2 = y_2;
+}
+
+Wall::~Wall()
+{
+	//Nothing to do yet.
+}
+
+void Wall::draw()
+{
+	//For kyle
+}
+
