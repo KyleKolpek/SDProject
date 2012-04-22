bdc7e23e135023c84d34007d35e0e85a3ced84da
Kyle Kolpek <kakolpek@gmail.com>
2012-04-22 18:54:22 -0400

    Corrected some comments and added SOIL

--- src/wall.cc
+++ src/wall.cc
@@ -1,10 +1,11 @@
+#include "SOIL/SOIL.h"
 #include "wall.h"
 
 Wall::Wall(float x1, float y1, float x2, float y2):
 	x1(x1),
+	y1(y1),
 	x2(x2),
-	x3(x3),
-	x4(x4)
+	y2(y2)
 {
 	
 	float tmpVertexData[] = {x1, 0.0, y1,
@@ -12,11 +13,11 @@ Wall::Wall(float x1, float y1, float x2, float y2):
 							 x2, 4.0, y2,
 							 x2, 0.0, y2};
     // Prepare vertex buffer
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, vertexCount * 3 * sizeof(float), vertexData,
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertexCount * 3 * sizeof(float), vertexData,
 		GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// Load texture data
 	texture = SOIL_load_OGL_texture(
