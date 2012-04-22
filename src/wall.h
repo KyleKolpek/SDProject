4c87389dfaea50e6b8a740ace19a7758074fdc0b
Shaden Smith <ShadenTSmith@gmail.com>
2012-04-22 15:00:42 -0400

    Moved to doxygen formatting on wall.h

--- src/wall.h
+++ src/wall.h
@@ -1,13 +1,34 @@
 #include "drawable.h"
 
+/***************************************************************************//**
+ * A wall to be drawn in our dungeon. 
+ * Each room in the dungeon has a list of walls. Walls are defined by two
+ * points, a start and end point.
+*******************************************************************************/
 class Wall : public Drawable
 {
 public:
+	/***********************************************************************//**
+	 * Wall constructor.
+	 * \param[in] x_1 y_1
+	 *     The starting point of the wall.
+	 * \param[in] x_2 y_2
+	 *     The ending point of the wall.
+	 **************************************************************************/
 	Wall(float x_1, float y_1, float x_2, float y_2);
+
+	/***********************************************************************//**
+	 * Wall destructor.
+	 * Currently empty.
+	 **************************************************************************/
 	~Wall();
 		
+	/***********************************************************************//**
+	 * Draws wall.
+	 **************************************************************************/
 	void draw();
 	
 private:
-	float x1, y1, x2, y2;
+	float x1, y1;	/** Starting point of wall. */
+	float x2, y2;	/** End point of wall. */
 };
