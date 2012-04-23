40be427f70dd20caa432a44f87a4243369074307
Shaden Smith <ShadenTSmith@gmail.com>
2012-04-22 23:06:23 -0400

    Fixed style issues with splashScreen.h and added include guards.

--- src/splashScreen.h
+++ src/splashScreen.h
@@ -1,15 +1,23 @@
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
+
 #include<SFML/window.hpp>
 #include<SFML/graphics.hpp>
 #include<SFML/system.hpp>
 
 /***************************************************************************//**
-* A class that just holds displays the Splash screnn.
-******************************************************************************/
+* A class that just displays the splash screen.
+*******************************************************************************/
 class SplashScreen
 {
-	public:
-		/***************************************************************************//**
-		* Actually displays the splash screen.
-		******************************************************************************/	
-		void Show(sf::RenderWindow &window);
+public:
+	/***********************************************************************//**
+	* Actually displays the splash screen.
+	* \param[in] window
+	*     Pass in the window to draw to.
+	***************************************************************************/
+	void Show(sf::RenderWindow &window);
 };
+
+#endif
+
