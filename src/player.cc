d56665667ecc5bfb2042995c31b6c412154233a4
Greg Early <greg@gregsaw.com>
2012-04-25 22:48:49 -0400

    Player movement is a result of total delta values instead of setting values individually

--- src/player.cc
+++ src/player.cc
@@ -25,19 +25,19 @@ void Player::update(float sec, sf::Input const &input)
 
 	if( input.IsKeyDown( sf::Key::A ) ) //move left
 	{
-		delta.x = -1;
+		delta.x -= 1;
 	}
 	if( input.IsKeyDown( sf::Key::S ) ) //move down
 	{
-		delta.z = 1;
+		delta.z += 1;
 	}
 	if( input.IsKeyDown( sf::Key::D ) ) //move right
 	{
-		delta.x = 1;
+		delta.x += 1;
 	}
 	if( input.IsKeyDown( sf::Key::W ) ) //move up
 	{
-		delta.z = -1;
+		delta.z -= 1;
 	}
 	if(glm::length(delta) == 0)
 	{
